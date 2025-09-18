c836c2e863b0955cda5617cc9d21b412d05f1213
enpiesie <48687327+enpiesie@users.noreply.github.com>
2025-09-18 19:42:55 +0200

    improved js formatting

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 
 // load saved data
-let savedClicks = localStorage.getItem("gurtClicks")
-savedClicks = parseInt(savedClicks, 10);
+let savedCoins = localStorage.getItem("gurtCoins");
+savedCoins = parseInt(savedCoins, 10);
 
 let savedRebirths = localStorage.getItem("gurtRebirths")
 savedRebirths = parseInt(savedRebirths, 10);
 
-let savedGoons = localStorage.getItem("gurtGoons")
+let savedGoons = localStorage.getItem("gurtGoons");
 savedGoons = parseInt(savedGoons, 10);
 
-let clicks = savedClicks || 0;
+let coins = savedCoins || 0;
 let rebirths = savedRebirths || 0;
 let goons = savedGoons || 0;
 
-const clicksLabel = document.getElementById("clickCount");
-const clickStatsLabel = document.getElementById("clickStats");
+// get html elements
+const coinsLabel = document.getElementById("clickCount");
+const coinStatsLabel = document.getElementById("incomeStats");
 const rebirthsLabel = document.getElementById("rebirthCount");
 const goonsLabel = document.getElementById("goonCount");
 
@@ -25,22 +26,16 @@ const goonBtn = document.getElementById("goonBtn");
 updateLabels();
 
 // formulas
-function calcRebirthCost() {
-    return 100 + Math.pow(rebirths, 2) * 30;
-}
-function calcClickMult() {
-    return 1 + (rebirths * 0.5);
-}
-function calcGoonIncome() {
-    return goons * calcClickMult();
-}
-function calcGoonCost() {
-    return 20 + Math.pow(goons, 2) * 10;
-}
+function calcRebirthCost() { return 100 + Math.pow(rebirths, 2) * 30; };
+function calcClickMult() { return 1 + (rebirths * 0.5); };
+function calcGoonIncome() { return goons * calcClickMult(); };
+function calcGoonCost() { return 20 + Math.pow(goons, 2) * 10; };
+
+// common funcs
 
 function updateLabels() {
-    clicksLabel.textContent = `${Math.floor(clicks).toFixed(0)} Dubloons`;
-    clickStatsLabel.textContent = `
+    coinsLabel.textContent = `${Math.floor(coins).toFixed(0)} Dubloons`;
+    coinStatsLabel.textContent = `
   [Multiplier: ${calcClickMult().toFixed(1)}x]
    [Goons: $${calcGoonIncome().toFixed(1)}/s]
   `;
@@ -50,24 +45,25 @@ function updateLabels() {
     goonBtn.textContent = `Buy Goon [$${calcGoonCost()}]`
 
     // enable/disable buttons if you can afford
-    rebirthBtn.disabled = clicks < calcRebirthCost();
-    goonBtn.disabled = clicks < calcGoonCost();
-}
+    rebirthBtn.disabled = coins < calcRebirthCost();
+    goonBtn.disabled = coins < calcGoonCost();
+};
 
 function saveData() {
-    localStorage.setItem("gurtClicks", clicks);
+    localStorage.setItem("gurtCoins", coins);
     localStorage.setItem("gurtRebirths", rebirths);
     localStorage.setItem("gurtGoons", goons);
-}
+};
 
 function resetData() {
-    clicks = 0;
+    coins = 0;
     rebirths = 0;
     goons = 0;
     saveData();
     updateLabels();
-}
+};
 
+// creates the "+$1.0" effect when earning coins
 function spawnClickFx() {
     const fx = document.createElement("p");
     fx.classList.add("fx");
@@ -84,45 +80,49 @@ function spawnClickFx() {
     setTimeout(function () {
         fx.remove();
     }, 500);
-}
+};
+
+// button callbacks:
 
 function gurtClick() {
-    clicks += calcClickMult();
+    coins += calcClickMult();
     saveData();
     updateLabels();
     spawnClickFx();
-}
+};
 
+// reset all stats but get income multiplier
 function Rebirth() {
     const cost = calcRebirthCost();
-    if (clicks < cost) { return };
-    clicks = 0;
+    if (coins < cost) { return };
+    coins = 0;
     goons = 0;
     rebirths += 1;
     saveData();
     updateLabels();
-}
+};
 
+// buy worker
 function buyGoon() {
     const cost = calcGoonCost();
-    if (clicks < cost) { return };
-    clicks -= cost;
+    if (coins < cost) { return };
+    coins -= cost;
     goons += 1;
     saveData();
     updateLabels();
-}
+};
 
-// run goons
+// run workers every 100ms (they auto click for you)
 const timeID = setInterval(runGoons, 100);
 let lastFullCashNum = 0;
 function runGoons() {
-    clicks += calcGoonIncome() * 0.1;
+    coins += calcGoonIncome() * 0.1;
 
-    if (Math.floor(clicks) > lastFullCashNum) {
-        lastFullCashNum = clicks;
+    if (Math.floor(coins) > lastFullCashNum) {
+        lastFullCashNum = coins;
         spawnClickFx();
     }
 
     saveData();
     updateLabels();
-}
+};
